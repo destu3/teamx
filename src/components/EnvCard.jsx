4447f5ded7422e49492b8c0e7d69044b137edf35
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2023-10-08 21:49:40 +0100

    added speakText to Home page where when hovering over each card, it says the title of the game.

--- src/components/EnvCard.jsx
+++ src/components/EnvCard.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
 import '../styles/EnvCard.css';
+import speakText from "./speakText";
+import {useEffect, useState} from "react";
 
 const EnvCard = ({ data, pni }) => {
   const { env, coverImage, sectionHeading } = data;
 
+  const [isHover, SetIsHover] = useState(false);
+    useEffect(() => {
+        if (isHover) speakText(sectionHeading);
+    },[isHover])
+
   return (
-    <section aria-label={`${env}-section`} className="env-wrapper">
+    <section aria-label={`${env}-section`} className="env-wrapper" onMouseEnter={() => SetIsHover(true)} onMouseLeave={() => SetIsHover(false)}>
       <Link
         to={"/" + env}
         style={{ backgroundImage: `url(${coverImage})` }}
